a604f794adb47e15983bf7617d0ef4c1f22c8923
JoyshreeDutta <duttajoyshree21@gmail.com>
2021-09-09 08:52:17 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Home from "./Components/Home";
-import "./Styles/Home.css";
-import "./Styles/ContactDetails.css";
-import "./Styles/FormStatus.css";
-import "./Styles/PersonalData.css";
-import "./Styles/CompanyData.css";
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home';
+import './Styles/Home.css';
+import './Styles/ContactDetails.css';
+import './Styles/FormStatus.css';
+import './Styles/PersonalData.css';
+import './Styles/CompanyData.css';
 import "antd/dist/antd.css";
 
 function App() {
